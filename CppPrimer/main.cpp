5f3c585a0eadffb08c7d1f554401117674d2944f
nathanHS <test@q.com>
2017-06-01 12:05:53 +0800

    Initial Commit

---
+++ CppPrimer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppPrimer
+//
+//  Created by Joe on 2017/6/1.
+//  Copyright © 2017年 蔡华帅. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
